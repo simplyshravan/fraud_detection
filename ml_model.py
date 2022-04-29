d973327c4142fd92585f77987f434e95d5a4169d
Shravan Yadav <shravan.simply@gmail.com>
2022-04-29 17:43:36 +0530

    modifed ml_model model2

--- ml_model.py
+++ ml_model.py
@@ -3,7 +3,7 @@ def model1():
     print("adding code to ml model1")
 
 def model2():
-    print("adding code to ml model2")
+    print("adding code to ml model1")
 
 def model3():
     print("adding code to ml model3")
